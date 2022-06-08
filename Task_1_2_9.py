6903aa8ac998be6adf9f1c2fbb97325d8395841c
Nik <baburovn@gmail.com>
2022-06-08 14:56:18 +0500

    [update][done]1_2_9

--- Task_1_2_9.py
+++ Task_1_2_9.py
@@ -4,6 +4,11 @@ class TravelBlog:
 tb1 = TravelBlog()
 setattr(tb1,"name", "Франция")
 setattr(tb1,"days", 6)
+TravelBlog.total_blogs += 1
 
+tb2 = TravelBlog()
+setattr(tb2,"name", "Италия")
+setattr(tb2,"days", 5)
+TravelBlog.total_blogs += 1
 
 
